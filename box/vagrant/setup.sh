6de73fef3cf0209420d44059616cad8e331fa690
ike709 <ike709@github.com>
2022-02-11 13:09:11 -0600

    CI Hell

--- box/vagrant/setup.sh
+++ box/vagrant/setup.sh
@@ -1,31 +1,59 @@
 
-sudo yum -y update 
-sudo yum -y install epel-release
-sudo yum -y install https://repo.ius.io/ius-release-el7.rpm 
-sudo yum -y update
+declare -A osInfo;
+osInfo[/etc/centos-release]=yum
+osInfo[/etc/os-release]=apt
 
-sudo yum -y groupinstall "Development Tools"
-sudo yum -y install openssl-devel bzip2-devel libffi-devel xz-devel
-sudo yum -y install wget
+declare packMan;
 
-sudo yum -y install glibc.i686
-sudo yum -y install libstdc++.i686
+for f in ${!osInfo[@]}
+do
+    if [[ -f $f ]];then
+        echo Package manager: ${osInfo[$f]}
+		packMan=${osInfo[$f]}
+    fi
+done
+
+if [[ $packMan == "yum" ]];then
+	sudo $packMan -y update 
+	sudo $packMan -y install epel-release
+	sudo $packMan -y install https://repo.ius.io/ius-release-el7.rpm 
+fi
+
+sudo $packMan -y update
+
+if [[ $packMan == "yum" ]];then
+	sudo $packMan -y groupinstall "Development Tools"
+	sudo $packMan -y install openssl-devel bzip2-devel libffi-devel xz-devel
+	sudo $packMan -y install wget
+else
+	sudo $packMan -y install gcc
+	sudo $packMan -y install libc6
+	sudo $packMan -y install libstdc++6
+	sudo $packMan -y install libssl-dev llvm libncurses5-dev libncursesw5-dev tk-dev python-openssl
+fi
+
 
 cd $BASE_DIR 
 wget https://www.python.org/ftp/python/3.8.12/Python-3.8.12.tgz
-tar xvf Python-3.8.12.tgz
-cd Python-3.8*
-./configure --enable-optimizations
+tar xf Python-3.8.12.tgz
+cd Python-3.8.12/
+if [[ $packMan == "yum" ]];then
+	./configure --enable-optimizations
+		
+fi
 sudo make altinstall
 sudo ln -s /usr/local/bin/python3.8 /bin/python3.8
 sudo python3.8 -m pip install --upgrade pip
 sudo python3.8 -m pip install GitPython
 
-sudo yum -y remove git
-sudo yum -y install git222
-
-sudo rpm -Uvh https://packages.microsoft.com/config/centos/7/packages-microsoft-prod.rpm
-sudo yum -y install dotnet-sdk-6.0
+if [[ $packMan == "yum" ]];then
+	sudo $packMan -y remove git
+	sudo $packMan -y install git222
+	sudo rpm -Uvh https://packages.microsoft.com/config/centos/7/packages-microsoft-prod.rpm
+	sudo $packMan -y install dotnet-sdk-6.0
+else
+	sudo $packMan -y install dotnet-runtime-6.0
+fi
 
 cd $BASE_DIR
 mkdir dream-storage
