a35884de2803c6ed826559ceb4800c6f0feff218
mumencoder <mumencoder@users.noreply.github.com>
2022-07-29 11:44:04 -0400

    move setup.py to toplevel

--- lib/setup.py
+++
@@ -1,7 +0,0 @@
-import os, setuptools
-
-setuptools.setup(
-    name = "mumen-dream-tools",
-    version = "0.1.0",
-    packages=setuptools.find_packages()
-)
---
+++ setup.py
@@ -0,0 +1,13 @@
+import os, setuptools
+
+setuptools.setup(
+    name = "mumen-dream-tools",
+    version = "1.0.0a0",
+    zip_safe = True,
+    packages=setuptools.find_packages(where='./lib'),
+    package_dir={
+        "DTT":"./lib/DTT",
+        "Shared":"./lib/Shared",
+        "dream_collider":"./lib/dream_collider"
+    }
+)
