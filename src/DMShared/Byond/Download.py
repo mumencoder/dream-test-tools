c481d8b0d5451afcdd4b37b8ae147279a78183f7
mumencoder <mumencoder@users.noreply.github.com>
2023-05-01 21:07:13 -0400

    byond archive downloader

--- src/DMShared/Byond/Download.py
+++ src/DMShared/Byond/Download.py
@@ -3,21 +3,84 @@ from ..common import *
 
 class Download(object):
     @staticmethod
-    def get_filenames(env):
-        filenames = {}
-        full_version = f"{env.attr.install.version.major}.{env.attr.install.version.minor}"
-        filenames["byondexe"] = f"{full_version}_byondexe.zip"
-        filenames["byond_setup"] = f"{full_version}_byond_setup.zip"
-        filenames["byond_linux"] = f"{full_version}_byond_linux.zip"
-        filenames["byond.exe"] = f"{full_version}_byond.exe"
-        filenames["byond.zip"] = f"{full_version}_byond.zip"
-        return filenames
+    def parse_official_filename(filename):
+        def readint(pos):
+            while pos < len(filename) and filename[pos].isnumeric():
+                pos += 1
+            return pos
+        
+        pos = 0
+        result = {"filename":filename}
+        if (new_pos := readint(pos)) != pos:
+            result["major"] = filename[pos:new_pos]
+            pos = new_pos
+        else:
+            return {"type":"unknown"}
+
+        if filename[pos] != ".":
+            return {"type":"unknown"}
+        pos += 1
+
+        if (new_pos := readint(pos)) != pos:
+            result["minor"] = filename[pos:new_pos]
+            pos = new_pos
+        else:
+            return {"type":"unknown"}
+
+        if filename[pos] != "_":
+            return {"type":"unknown"}
+        pos += 1
+
+        if filename[pos:] in [
+            "byond_freebsd.zip", "byond_linux.zip",
+            "byondexe.zip", "byond_setup.zip", "byond.zip", "byond.exe",
+            "byond_netbsd.zip", "byond_openbsd.zip", "byond_linux_old_glibc.zip",
+            "byond_macosx.zip",
+            "byond_mfc120.zip",
+            ]:
+            result["type"] = filename[pos:]
+            return result
+        else:
+            return {"type":"unknown"}
 
     @staticmethod
-    async def linux(env):
-        filename = f"{env.attr.install.version.major}.{env.attr.install.version.minor}_byond_linux.zip"
-        url = f'https://www.byond.com/download/build/{env.attr.install.version.major}/{filename}'
-        if not os.path.exists(env.attr.install.save_path):
-            penv = env.branch()
-            penv.attr.shell.command = f"wget {url} -O {env.attr.install.save_path}"
-            await Shared.Process.shell(penv)
+    def from_official_source(env):
+        env.attr.byond.version_directory_url = "https://www.byond.com/download/build/"
+
+    @staticmethod
+    def fetch_version_directory(env):
+        result = requests.get(env.attr.byond.version_directory_url)
+        if result.status_code == 200:
+            env.attr.byond.version_directory = requests.get(env.attr.byond.version_directory_url).content
+        else:
+            raise Exception(result.status_code)
+        
+    @staticmethod
+    def parse_apache_dir(env, page):
+        soup = BeautifulSoup(page, 'html.parser')
+
+        for link in soup.find_all('pre')[0].find_all('a'):
+            yield link
+
+    @staticmethod
+    def iter_version_directories(env):
+        for link in Download.parse_apache_dir(env, env.attr.byond.version_directory):
+            if link['href'].startswith('?') or link['href'].startswith('/'):
+                continue
+            if not link['href'][0].isnumeric():
+                continue
+            yield link
+
+    @staticmethod
+    def fetch_version_files(env):
+        for link in Download.iter_version_directories(env):
+            url = env.attr.byond.version_directory_url + link['href']
+            result = requests.get(url)
+            if result.status_code == 200:                
+                for link in Download.parse_apache_dir(env, result.content):
+                    if link['href'].startswith('?') or link['href'].startswith('/'):
+                        continue
+                    yield {"url":url + link['href'], "file":link['href']}
+            else:
+                raise Exception(result.status_code)
+            time.sleep(2.0)
--- src/DMShared/Byond/Install.py
+++ src/DMShared/Byond/Install.py
@@ -10,10 +10,8 @@ class Install(object):
         if not os.path.exists(env.attr.install.dir):
             penv = env.branch()
             penv.attr.shell.env = os.environ
-            penv.attr.shell.command = f"unzip -qq {env.attr.install.save_path} -d {env.attr.install.dir}"
+            penv.attr.shell.command = f"unzip -qq {env.attr.install.zip_path} -d {env.attr.install.dir}"
             await Shared.Process.shell(penv)
-            if os.path.exists( env.attr.install.save_path ):
-                os.remove( env.attr.install.save_path )
 
     @staticmethod
     async def available(env):
